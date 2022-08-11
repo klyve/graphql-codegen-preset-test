c4ba9eeb2a460a2a0c1a62918437e7c04db08551
Bjarte Klyve Larsen <bjarteklarsen@gmail.com>
2022-08-11 10:46:43 +0200

    minor changes

--- run-tests.sh
+++ run-tests.sh
@@ -1,15 +1,15 @@
 # Setup the tests by generating the required files
-pnpm test:setup > /dev/null 2>&1
+yarn test:setup > /dev/null 2>&1
 
 echo "* Testing near-operation generation with introspection json"
-pnpm test:introspection > /dev/null
+yarn test:introspection > /dev/null
 if [ $? -ne 0 ]; then
   echo "Introspection test failed"
   exit 1
 fi
 
 echo "* Testing near-operation generation with schema"
-pnpm test:schema > /dev/null
+yarn test:schema > /dev/null
 if [ $? -ne 0 ]; then
   echo "Introspection test failed"
   exit 1
@@ -18,14 +18,14 @@ fi
 # Run just the operations generation
 # This should fail so if it passes thats a problem
 echo "* Testing operations generation"
-pnpm test:operations > /dev/null 2>&1
+yarn test:operations > /dev/null 2>&1
 if [ $? -ne 1 ]; then
   echo "Operations generation passed even though it should have failed"
   exit 1
 fi
 
 # Cleanup / Remove all the generated files
-pnpm test:cleanup > /dev/null
+yarn test:cleanup > /dev/null
 
 
 echo "Passed all tests"
